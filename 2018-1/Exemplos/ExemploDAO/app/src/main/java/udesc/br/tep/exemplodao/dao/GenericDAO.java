306e6abb8a7f8e3cf82e24ce74136119a0c2ef77
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-03-22 21:41:09 -0300

    Aula 5 - Manipulando arquivos e padrão DAO

---
+++ 2018-1/Exemplos/ExemploDAO/app/src/main/java/udesc/br/tep/exemplodao/dao/GenericDAO.java
@@ -0,0 +1,13 @@
+package udesc.br.tep.exemplodao.dao;
+
+import java.util.List;
+
+import udesc.br.tep.exemplodao.service.Arquivo;
+
+public interface GenericDAO<O> {
+    void inserir(O objeto);
+    void alterar(O objeto);
+    void exluir(int id);
+    O buscar(int id);
+    List<O> lista();
+}
