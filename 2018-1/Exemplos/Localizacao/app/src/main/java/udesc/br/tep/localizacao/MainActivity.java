5bfdf1dd2b625cfd6b29ffe75bc399960fc668be
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-05-10 19:59:03 -0300

    Aula 9 - Sensores e Localização

---
+++ 2018-1/Exemplos/Localizacao/app/src/main/java/udesc/br/tep/localizacao/MainActivity.java
@@ -0,0 +1,65 @@
+package udesc.br.tep.localizacao;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient mFusedLocationClient;
+    private TextView txtLocalizacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtLocalizacao = findViewById(R.id.txtLocalizacao);
+
+        // Mostrar a distancia entre 2 pontos
+        float results[] = new float[3];
+        Location.distanceBetween(
+                -26.242910,
+                -49.512308,
+                -26.239751,
+                -49.506260,
+                results
+        );
+        System.out.println("Distância: " + results[2]);
+
+        // Provedor de localização do Android
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    100);
+        } else {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Obtem a última localizacação,
+                            // porém devemos verificar se é nula (só em alguns casos ocorre)
+                            // Ex: localização desabilitada no dispositivo
+                            if (location != null) {
+                                // Trabalhar com a localização
+                                txtLocalizacao.setText(location.toString());
+                            }
+                        }
+                    });
+
+        }
+
+
+    }
+}
---
+++ 2018-1/Exemplos/Sensores/app/src/main/java/udesc/br/tep/sensores/MainActivity.java
@@ -0,0 +1,53 @@
+package udesc.br.tep.sensores;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mLight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Sensor Manager e o sensor usado
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Aqui deve tratar a precisão
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // O sensor de luminosidade retorna um valor único.
+        // A maioria dos sensores retorna três valores (um pra cada eixo)
+
+        float lux = event.values[0];
+        System.out.println("Luminosidade: " + lux);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+}
