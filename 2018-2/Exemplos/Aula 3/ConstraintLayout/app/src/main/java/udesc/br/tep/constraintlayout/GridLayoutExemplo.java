9929836eb96e65b5c14c4e8b67d22126c33eacc2
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-08-16 21:28:42 -0300

    Exemplos da aula 3

---
+++ 2018-2/Exemplos/Aula 3/ConstraintLayout/app/src/main/java/udesc/br/tep/constraintlayout/GridLayoutExemplo.java
@@ -0,0 +1,13 @@
+package udesc.br.tep.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridLayoutExemplo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout_exemplo);
+    }
+}
---
+++ 2018-2/Exemplos/Aula 3/ConstraintLayout/app/src/main/java/udesc/br/tep/constraintlayout/LinearLayoutHExemplo.java
@@ -0,0 +1,13 @@
+package udesc.br.tep.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayoutHExemplo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout_hexemplo);
+    }
+}
