c9c0e50305ed07bae3a680f7c19079648fbfc53e
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2018-10-04 21:14:06 -0300

    Aula 8 - Threads, Handler, Async e Sockets

---
+++ 2018-2/Exemplos/Aula 8/DownloadImagemThread/app/src/main/java/udesc/br/tep/downloadimagemthread/MainActivity.java
@@ -0,0 +1,139 @@
+package udesc.br.tep.downloadimagemthread;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String imagemURL = "https://www.hdwallpapersfreedownload.com/uploads/large/cars/bmw-m6-full-hd.jpg";
+    private ImageView imgDownload = null;
+    private ProgressBar pbDownload = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgDownload = findViewById(R.id.imgDownload);
+        pbDownload = findViewById(R.id.pbDownload);
+        pbDownload.setIndeterminate(true);
+
+        // downloadImagem(imagemURL);
+        //downloadImagemContinuo(imagemURL);
+        downloadImagemComAsync(imagemURL);
+    }
+
+    private void downloadImagem(final String imgURL){
+        Thread thread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        // Faz o download - thread separada
+                        final Bitmap bitmap = loadImageFromNetwork(imgURL);
+
+                        // Atualiza o componente de imagem - thread separada
+                        imgDownload.post(new Runnable() {
+                            public void run() {
+                                imgDownload.setImageBitmap(bitmap);
+                                pbDownload.setVisibility(View.INVISIBLE);
+                            }
+                        });
+                    }
+                }
+        );
+
+        thread.start();
+    }
+
+    private void downloadImagemContinuo(final String imgURL){
+        // Dispara a thread
+        Thread thread = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        // Zerar a img
+                        imgDownload.setImageBitmap(null);
+
+                        // Exibe a progressbar
+                        pbDownload.setVisibility(View.VISIBLE);
+
+                        // Faz o download - thread separada
+                        final Bitmap bitmap = loadImageFromNetwork(imgURL);
+                        System.out.println("Terminei o download!");
+
+                        // Atualiza o componente de imagem - thread separada - 10s
+                        imgDownload.postDelayed(new Runnable() {
+                            public void run() {
+                                imgDownload.setImageBitmap(bitmap);
+                                pbDownload.setVisibility(View.INVISIBLE);
+                            }
+                        }, 10000);
+                    }
+                }
+        );
+
+        thread.start();
+
+    }
+
+    private void downloadImagemComAsync(final String imgURL){
+        // Cria uma ASyncTask
+        DownloadFilesTask down_async = new DownloadFilesTask();
+
+        // Executa uma task
+        down_async.execute();
+    }
+
+    private Bitmap loadImageFromNetwork(String src) {
+        try {
+            // Habilita barra
+            pbDownload.setVisibility(View.VISIBLE);
+
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            Bitmap icon = BitmapFactory.decodeResource(getBaseContext().getResources(), R.mipmap.ic_launcher);
+            e.printStackTrace();
+            return icon;
+        }
+    }
+
+    private class DownloadFilesTask extends AsyncTask<Void, Void, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Mostra o ProgressBar para fazer a animacao
+            pbDownload.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            // Faz o download da imagem
+            Bitmap bitmap = loadImageFromNetwork(imagemURL);
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null) {
+                pbDownload.setVisibility(View.INVISIBLE);
+                imgDownload.setImageBitmap(bitmap);
+            }
+        }
+    }
+}
---
+++ 2018-2/Exemplos/Aula 8/ExemploThreadHandlerAsync/app/src/main/java/udesc/br/tep/exemplothreadhandlerasync/MeuHandler.java
@@ -0,0 +1,20 @@
+package udesc.br.tep.exemplothreadhandlerasync;
+
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+
+/**
+ * Created by luis on 06/03/18.
+ */
+
+public class MeuHandler extends Handler {
+    @Override
+    public void handleMessage(Message msg) {
+        switch(msg.what){
+            case MinhaConstante.MENSAGEM_TESTE: System.out.println("Chegou"); break;
+            default: System.out.println("Não conheco a mensagem!");
+        }
+
+    }
+}
